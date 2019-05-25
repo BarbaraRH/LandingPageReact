7d00e1c2b656509690fda8b340a31b1c69ef33f8
pablochile2017 <pablochile2017@gmail.com>
2019-05-25 18:57:29 +0000

    jumbotron

---
+++ hello-rigo/src/js/component/jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//create your first component
+const Jumbotron = () => {
+	return (
+		<header className="jumbotron my-4">
+			<h1 className="display-3"> A Warm Welcome </h1>
+			<p className="lead">Lorem ipsum</p>
+			<a href="#" className="btn btn-primary btn-lg">
+				Call to action!
+			</a>
+		</header>
+	);
+};
+
+export default Jumbotron;
